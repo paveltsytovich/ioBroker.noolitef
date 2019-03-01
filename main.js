85c3f29ac281a0f62d521fa5793061fa73d42d0f
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-03-01 14:22:21 +0300

    Ready for backend development

--- main.js
+++ main.js
@@ -32,53 +32,12 @@ class Noolitef extends utils.Adapter {
 	 * Is called when databases are connected and adapter received configuration.
 	 */
 	async onReady() {
-		// Initialize your adapter here
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info('config option1: ' + this.config.option1);
-		this.log.info('config option2: ' + this.config.option2);
-
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectAsync('testVariable', {
-			type: 'state',
-			common: {
-				name: 'testVariable',
-				type: 'boolean',
-				role: 'indicator',
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
+		//TO DO
 
 		// in this template all states changes inside the adapters namespace are subscribed
 		this.subscribeStates('*');
 
-		/*
-		setState examples
-		you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync('testVariable', true);
-
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync('testVariable', { val: true, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync('admin', 'iobroker');
-		this.log.info('check user admin pw ioboker: ' + result);
-
-		result = await this.checkGroupAsync('admin', 'admin');
-		this.log.info('check group user admin group admin: ' + result);
 	}
 
 	/**
@@ -100,13 +59,15 @@ class Noolitef extends utils.Adapter {
 	 * @param {ioBroker.Object | null | undefined} obj
 	 */
 	onObjectChange(id, obj) {
-		if (obj) {
-			// The object was changed
-			this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-		} else {
-			// The object was deleted
-			this.log.info(`object ${id} deleted`);
-		}
+
+		//TO DO
+		// if (obj) {
+		// 	// The object was changed
+		// 	this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+		// } else {
+		// 	// The object was deleted
+		// 	this.log.info(`object ${id} deleted`);
+		// }
 	}
 
 	/**
@@ -115,13 +76,16 @@ class Noolitef extends utils.Adapter {
 	 * @param {ioBroker.State | null | undefined} state
 	 */
 	onStateChange(id, state) {
-		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-		} else {
-			// The state was deleted
-			this.log.info(`state ${id} deleted`);
-		}
+
+        //TO DO
+
+		// if (state) {
+		// 	// The state was changed
+		// 	this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+		// } else {
+		// 	// The state was deleted
+		// 	this.log.info(`state ${id} deleted`);
+		// }
 	}
 
 	/**
@@ -131,17 +95,17 @@ class Noolitef extends utils.Adapter {
 	 */
 	onMessage(obj) {
 		this.log.info(JSON.stringify(obj));
-		if (typeof obj === "object" && obj.message) {
-			if (obj.command === "Bind") {
+		if (typeof obj === 'object' && obj.message) {
+			if (obj.command === 'Bind') {
 				// e.g. send email or pushover or whatever
-				this.log.info("Bind command");
+				this.log.info('Bind command');
 
 				// Send response in callback if required
-				if (obj.callback) this.sendTo(obj.from, obj.command, "OK", obj.callback);
+				if (obj.callback) this.sendTo(obj.from, obj.command, 'OK', obj.callback);
 			}
-			else if(obj.command == "Unbind") {
-				this.log.info("Unbind command");
-				if (obj.callback) this.sendTo(obj.from, obj.command, "OK", obj.callback);
+			else if(obj.command == 'Unbind') {
+				this.log.info('Unbind command');
+				if (obj.callback) this.sendTo(obj.from, obj.command, 'OK', obj.callback);
 
 			}
 		}
@@ -149,6 +113,7 @@ class Noolitef extends utils.Adapter {
 
 }
 
+// @ts-ignore
 if (module.parent) {
 	// Export the constructor in compact mode
 	/**
