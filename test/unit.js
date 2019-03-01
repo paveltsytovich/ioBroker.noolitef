46688fda02e09704506e8d7a6b9e0000eaaf7504
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-03-01 12:28:35 +0300

    Solve problem for unit testing

--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,13 @@
 const path = require('path');
-const { tests } = require('@iobroker/testing');
+const { tests,utils } = require('@iobroker/testing');
+
+const { adapter, database } = utils.unit.createMocks();
+const { assertObjectExists } = utils.unit.createAsserts(database, adapter);
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, '..'));
+tests.unit(path.join(__dirname, '..'), {
+    allowedExitCodes: [11],
+    defineAdditionalTests() {
+        
+    }
+});
