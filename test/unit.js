0fa2c07f852e15b2db2d833b515cb3b675667f54
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-03-24 09:28:34 +0300

    Add new feature of unit test

--- test/unit.js
+++ test/unit.js
@@ -1,17 +1,24 @@
 const path = require('path');
 const { tests,utils } = require('@iobroker/testing');
+// @ts-ignore
 const FakeSerialPort = require('serialport/test');
 
 FakeSerialPort.Binding.createPort('/dev/ttyUSB0',{ echo: false, record: true,autoOpen: false });
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
+
+// @ts-ignore
 tests.unit(path.join(__dirname, '..'), {
 	allowedExitCodes: [11],
 	additionalMockedModules: {
 		'serialport': FakeSerialPort
 	},
+	overwriteAdapterConfig(config) {
+		config.devices[0] = {'1':'2'};
+
+	},
 	defineAdditionalTests() {
+	    // @ts-ignore
 	    const { adapter, database } = utils.unit.createMocks();
-		const { assertObjectExists } = utils.unit.createAsserts(database, adapter);
 		it('work', () => {
 			adapter.setForeignState('noolitef.0',{val:2, ack: true});
 			//adapter.setForeignStateChanged('noolitef.0');
@@ -19,7 +26,7 @@ tests.unit(path.join(__dirname, '..'), {
 			
 		});        
 	},
-	defineMockBehavior(db,adapter) {
-		//console.log(JSON.stringify(adapter));
+	defineMockBehavior(adapter) {
+		console.log(JSON.stringify(adapter));
 	}
 });
